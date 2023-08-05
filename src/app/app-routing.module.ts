c68075d32702440509e73ec7064e94ab99b269c3
Ivaylo Anev <ivaylo.anev1@gmail.com>
2023-08-05 15:32:40 +0300

    contacts routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 const routes: Routes = [
   {
@@ -17,6 +18,10 @@ const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent,
+  },
+  {
+    path: 'contacts',
+    component: ContactsComponent,
   },
   //{ path: '**', component: NotFoundComponent },
 ];
