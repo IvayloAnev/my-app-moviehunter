624e7e85d9ed9afb36e073b1e4b7cd1efbc89fd8
Ivaylo Anev <ivaylo.anev1@gmail.com>
2023-08-07 17:14:33 +0300

    current-movie.component

--- src/app/movie/current-movie/current-movie.component.ts
+++ src/app/movie/current-movie/current-movie.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { Movie } from 'src/app/types/movie';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-current-movie',
   templateUrl: './current-movie.component.html',
-  styleUrls: ['./current-movie.component.css']
+  styleUrls: ['./current-movie.component.css'],
 })
-export class CurrentMovieComponent {
+export class CurrentMovieComponent implements OnInit {
+  movie: Movie | undefined;
 
+  constructor(
+    private apiService: ApiService,
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService
+  ) {}
+
+  get isLogged(): boolean {
+    return this.userService.isLogged;
+  }
+
+  ngOnInit(): void {
+    this.fetchMovie();
+  }
+
+  fetchMovie(): void {
+    const id = this.activatedRoute.snapshot.params['movieId'];
+
+    this.apiService.getMovie(id).subscribe((movie) => {
+      this.movie = movie;
+      console.log({ movie });
+    });
+  }
 }
