c3c0c5fd2dd4c11de8e78477374f4dfaf0f804a2
Ivaylo Anev <ivaylo.anev1@gmail.com>
2023-08-09 20:14:08 +0300

    change regExp in validator

--- src/app/shared/validators/app-email.validator.ts
+++ src/app/shared/validators/app-email.validator.ts
@@ -2,7 +2,8 @@ import { ValidatorFn } from "@angular/forms";
 
 export function appEmailValidator(domains: string[]): ValidatorFn {
   const domainStrings = domains.join("|"); // ['bg', 'com'] => bg|com
-  const regExp = new RegExp(`[^@]{4,}@gmail\.(${domainStrings})$`);
+  //const regExp = new RegExp(`[^@]{4,}@gmail\.(${domainStrings})$`);
+    const regExp = new RegExp(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
 
   return (control) => {
     const x = control.value;
